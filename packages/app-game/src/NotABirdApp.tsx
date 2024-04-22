2160a9cb148aeed59bbb264a67ee7c127b100495
Frank Dierolf <frank_dierolf@web.de>
2024-04-22 18:00:39 +0200

    :art: (app-game) npm run format

--- packages/app-game/src/NotABirdApp.tsx
+++ packages/app-game/src/NotABirdApp.tsx
@@ -6,13 +6,13 @@ import { GameOverPage } from "./pages/GameOverPage";
 import { StarMapPage } from "./pages/StarMapPage";
 import { StarSystemPage } from "./pages/StarSystemPage";
 
-const pages: Array<{ path: string, component: any }> = [
-    { path: "/landing-page", component: LandingPage },
-    { path: "/star-map", component: StarMapPage },
-    { path: "/star-system", component: StarSystemPage },
-    { path: "/game", component: GamePage },
-    { path: "/game-over", component: GameOverPage },
-    { path: "/frank", component: FrankPage },
+const pages: Array<{ path: string; component: any }> = [
+  { path: "/landing-page", component: LandingPage },
+  { path: "/star-map", component: StarMapPage },
+  { path: "/star-system", component: StarSystemPage },
+  { path: "/game", component: GamePage },
+  { path: "/game-over", component: GameOverPage },
+  { path: "/frank", component: FrankPage },
 ];
 
 export function NotABirdApp() {
