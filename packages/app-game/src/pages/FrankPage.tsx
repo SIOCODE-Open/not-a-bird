8283d4bb55dfdcd4df1583ba40a92db989ff9f83
Frank Dierolf <frank_dierolf@web.de>
2024-04-25 16:33:00 +0200

    :zap: (app-game) add getStone(x) mintStone(x), implement it

--- packages/app-game/src/pages/FrankPage.tsx
+++ packages/app-game/src/pages/FrankPage.tsx
@@ -4,20 +4,30 @@ import { useFrank } from "../service/FrankService";
 
 export function FrankPage(props: { navigate: (path: string) => void }) {
   const [mintNumber, setMintNumber] = useState(new BN(2));
-  const { mintRock, getRockOwnerOf } = useFrank();
+  const { mintRock, getRockOwnerOf, getStoneOwnerOf, mintStone } = useFrank();
 
   return (
     <>
       <div className="flex justify-content column align-center">
         <h1>This is a frank Page</h1>
+
         <button onClick={async () => getRockOwnerOf(mintNumber)}>
+          Get Rock({mintNumber.toString()})
+        </button>
+        <br />
+        <button onClick={async () => mintRock(mintNumber)}>Mint Rock</button>
+        <br />
+        <hr style={{ borderTop: "1px solid black", width: "200px" }}></hr>
+
+        <br />
+        <button onClick={async () => getStoneOwnerOf(mintNumber)}>
           Get Stone({mintNumber.toString()})
         </button>
         <br />
-        <button onClick={async () => mintRock(mintNumber)}>Mint Stone</button>
+        <button onClick={async () => mintStone(mintNumber)}>Mint Stone</button>
         <br />
         <hr style={{ borderTop: "1px solid black", width: "200px" }}></hr>
-        <button>Mint Rock</button>
+
         <br />
         <button>Create Gem</button>
       </div>
--- packages/app-game/src/service/FrankService.ts
+++ packages/app-game/src/service/FrankService.ts
@@ -30,7 +30,7 @@ export function useFrank() {
     );
 
     console.log(
-      `Rock(${mintNumber.toString()}) is owned by ${output.toJSON()["ok"]}`,
+      `Rock (${mintNumber.toString()}) is owned by ${output.toJSON()["ok"]}`,
     );
   }
 
@@ -64,9 +64,73 @@ export function useFrank() {
       mintNumber,
     );
     console.log(
-      `Rock(${mintNumber.toString()}) is minted by ${output.toJSON()["ok"]}`,
+      `Rock (${mintNumber.toString()}) is minted by ${output.toJSON()["ok"]}`,
     );
   }
 
-  return { getRockOwnerOf, mintRock };
+  async function getStoneOwnerOf(mintNumber: BN) {
+    const wsProvider = new WsProvider("ws://127.0.0.1:9944");
+    const api = await ApiPromise.create({ provider: wsProvider });
+    const keyring = new Keyring({ type: "sr25519" });
+    const alicePair = keyring.addFromUri("//Alice");
+    const contractAdress = "5G8tYCMLZQYgwE9hkYNq5orJXEDhTCMu6kRa5frW4nY6bVsS";
+    const res = await fetch("./rock.json");
+    const abi = await res.json();
+
+    const contract = new ContractPromise(api, abi, contractAdress);
+    const gasLimit: WeightV2 = api.registry.createType("WeightV2", {
+      refTime: new BN("2000000000"),
+      proofSize: new BN("200000"),
+    });
+    const storageDepositLimit = null;
+
+    const { output } = await contract.query.ownerOf(
+      alicePair.address,
+      {
+        gasLimit,
+        storageDepositLimit,
+      },
+      new BN(mintNumber),
+    );
+
+    console.log(
+      `Stone (${mintNumber.toString()}) is owned by ${output.toJSON()["ok"]}`,
+    );
+  }
+
+  async function mintStone(mintNumber: BN) {
+    const wsProvider = new WsProvider("ws://127.0.0.1:9944");
+    const api = await ApiPromise.create({ provider: wsProvider });
+    const res = await fetch("./stone.json");
+    const abi = await res.json();
+    const contractAdress = "5G8tYCMLZQYgwE9hkYNq5orJXEDhTCMu6kRa5frW4nY6bVsS";
+    const contract = new ContractPromise(api, abi, contractAdress);
+
+    const keyring = new Keyring({ type: "sr25519" });
+    const alicePair = keyring.addFromUri("//Alice");
+
+    const gasLimit: WeightV2 = api.registry.createType("WeightV2", {
+      refTime: new BN("2000000000"),
+      proofSize: new BN("200000"),
+    });
+    const storageDepositLimit = null;
+
+    await contract.tx
+      .mint({ gasLimit, storageDepositLimit }, mintNumber)
+      .signAndSend(alicePair, () => {});
+
+    const { output } = await contract.query.ownerOf(
+      alicePair.address,
+      {
+        gasLimit,
+        storageDepositLimit,
+      },
+      mintNumber,
+    );
+    console.log(
+      `Stone (${mintNumber.toString()}) is minted by ${output.toJSON()["ok"]}`,
+    );
+  }
+
+  return { getRockOwnerOf, mintRock, getStoneOwnerOf, mintStone };
 }
