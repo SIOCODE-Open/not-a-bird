230ac867c4a875d416c8297f73c7875733047c1a
Frank Dierolf <frank_dierolf@web.de>
2024-04-29 10:45:20 +0200

    :zap: (contracts) adjust element

--- packages/contracts/element/lib.rs
+++ packages/contracts/element/lib.rs
@@ -2,50 +2,58 @@
 
 #[ink::contract]
 mod element {
-    use franks_interfaces::NftInterfacec;
+    use franks_interfaces::franks_interfaces::NftInterfacec;
+    use ink::storage::Mapping;
+
+    type TokenId = u128;
+    type TokenBalance = u128;
 
     #[ink(storage)]
+    #[derive(Default)]
     pub struct Element {
-        value: bool,
+        token_owner: Mapping<TokenId, AccountId>,
+        owned_tokens_count: Mapping<AccountId, u128>,
+        mint_count: u128,
     }
 
     impl Element {
         #[ink(constructor)]
-        pub fn new(init_value: bool) -> Self {
-            Self { value: init_value }
-        }
-
-        #[ink(constructor)]
-        pub fn default() -> Self {
-            Self::new(Default::default())
-        }
-
-        #[ink(message)]
-        pub fn flip(&mut self) {
-            self.value = !self.value;
-        }
-
-        #[ink(message)]
-        pub fn get(&self) -> bool {
-            true
+        pub fn new() -> Self {
+            Default::default()
         }
     }
 
     impl NftInterfacec for Element {
         #[ink(message)]
-        fn mint(&self) {}
+        fn mint(&mut self) {
+            // add ID to AccountId
+            let mint_count = self.mint_count;
+            let caller = self.env().caller();
+            self.token_owner.insert(mint_count, &caller);
+
+            // add count to AccountId
+            let current_count = self.owned_tokens_count.get(caller);
+            let updated_count = current_count.unwrap().saturating_sub(1);
+            self.owned_tokens_count.insert(caller, &updated_count);
+
+            let _ = self.mint_count.saturating_add(1);
+        }
+
+        // TODO
+        #[ink(message)]
+        fn burn(&mut self) {}
 
         #[ink(message)]
-        fn burn(&self) {}
-
-        #[ink(message)]
-        fn balance_of(&self, _owner: AccountId, _token_id: u32) -> Balance {
-            50u128
+        fn balance_of(&self, owner: AccountId) -> TokenBalance {
+            self.owned_tokens_count.get(owner).unwrap_or_default()
         }
 
         #[ink(message)]
-        fn transfer(&mut self, _to: AccountId, _value: u32) -> Result<(), ()> {
-            Ok(())
+        fn owner_of(&self, id: TokenId) -> AccountId {
+            self.token_owner.get(id).unwrap()
         }
+
+        #[ink(message)]
+        fn transfer(&mut self, _to: AccountId, _value: u32) {}
     }
 }
--- packages/contracts/franks_interfaces/lib.rs
+++ packages/contracts/franks_interfaces/lib.rs
@@ -1,23 +1,24 @@
 #![cfg_attr(not(feature = "std"), no_std, no_main)]
 
-pub use self::franks_interfaces::NftInterfacec;
-
 pub mod franks_interfaces {
     use ink::primitives::AccountId;
-    type Balance = u128;
+    type TokenBalance = u128;
 
     #[ink::trait_definition]
     pub trait NftInterfacec {
         #[ink(message)]
-        fn mint(&self);
+        fn mint(&mut self);
 
         #[ink(message)]
-        fn burn(&self);
+        fn burn(&mut self);
 
         #[ink(message)]
-        fn balance_of(&self, owner: AccountId, token_id: u32) -> Balance;
+        fn balance_of(&self, owner: AccountId) -> TokenBalance;
 
         #[ink(message)]
-        fn transfer(&mut self, to: AccountId, value: u32) -> Result<(), ()>;
+        fn owner_of(&self, id: u128) -> AccountId;
+
+        #[ink(message)]
+        fn transfer(&mut self, to: AccountId, value: u32);
     }
 }
