0f796f05fd9de2c1fcc0cfd685c0ae00722c7b2c
Frank Dierolf <frank_dierolf@web.de>
2024-05-04 17:54:41 +0200

    :zap: (app-game) add toggli picture

--- packages/app-game/src/pages/MergicanPage.tsx
+++ packages/app-game/src/pages/MergicanPage.tsx
@@ -2,7 +2,31 @@ import { useEffect, useState } from "react";
 import { useControls, button } from "leva";
 
 export function MergicanPage(props: { navigate: (path: string) => void }) {
-  const [imageSrc, setImageSrc] = useState("/assets/items/item.steelbars.png");
+  const [currentItemIndex, setCurrentItemIndex] = useState(0);
+  const [items, setItems] = useState([
+    "/assets/items/item.coalbag.png",
+    "/assets/items/item.coalore.png",
+    "/assets/items/item.corn.png",
+    "/assets/items/item.crystal.png",
+    "/assets/items/item.firewood.png",
+    "/assets/items/item.flour.png",
+    "/assets/items/item.gears.png",
+    "/assets/items/item.goldbars.png",
+    "/assets/items/item.goldore.png",
+    "/assets/items/item.ironore.png",
+    "/assets/items/item.logs.png",
+    "/assets/items/item.planks.png",
+    "/assets/items/item.popcorn.png",
+    "/assets/items/item.steelbars.png",
+    "/assets/items/item.wheat.png",
+  ]);
+  function handleClick() {
+    if (currentItemIndex < items.length - 1) {
+      setCurrentItemIndex((prevIndex) => prevIndex + 1);
+    } else {
+      setCurrentItemIndex(0);
+    }
+  }
   const [, set] = useControls(
     "a_number",
     () => ({
@@ -13,7 +37,7 @@ export function MergicanPage(props: { navigate: (path: string) => void }) {
       set_number: button(() => set_number()),
       change_picture: button(() => handleClick()),
     }),
-    [],
+    [currentItemIndex],
   );
   const set_number = async () => {
     set({ owner: 3 });
@@ -22,10 +46,6 @@ export function MergicanPage(props: { navigate: (path: string) => void }) {
   const [rectX, setRectX] = useState(0); // Initial X position of the rectangle
   const [rectY, setRectY] = useState(0); // Initial Y position of the rectangle
   const [isDragging, setIsDragging] = useState(false);
-
-  const handleClick = () => {
-    setImageSrc("/assets/items/item.gears.png"); // Change the image source on click
-  };
 
   const handleMouseDown = () => {
     setIsDragging(true);
@@ -43,7 +63,6 @@ export function MergicanPage(props: { navigate: (path: string) => void }) {
   };
 
   useEffect(() => {
-    console.log(imageSrc);
     const canvas = document.querySelector("canvas");
     const ctx = canvas.getContext("2d");
 
@@ -51,9 +70,11 @@ export function MergicanPage(props: { navigate: (path: string) => void }) {
     canvas.height = window.innerHeight;
 
     const texture = new Image();
-    texture.src = imageSrc;
+    texture.src = items[currentItemIndex];
+    console.log(currentItemIndex);
 
     texture.onload = () => {
+      ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear
       ctx.drawImage(texture, rectX, rectY, 50, 50); // Draw texture on canvas
     };
 
@@ -65,7 +86,7 @@ export function MergicanPage(props: { navigate: (path: string) => void }) {
       canvas.removeEventListener("mouseup", handleMouseUp);
       canvas.removeEventListener("mousemove", handleMouseMove);
     };
-  }, [imageSrc, rectX, rectY, isDragging]);
+  }, [currentItemIndex, rectX, rectY, isDragging]);
   return (
     <>
       <canvas></canvas>
