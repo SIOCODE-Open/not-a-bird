8ac96e6c5e68e5b4d6a8cf2a426032072f2dfc9a
Frank Dierolf <frank_dierolf@web.de>
2024-05-04 16:44:46 +0200

    :zap: (app-game) implent move function

--- packages/app-game/src/pages/MergicanPage.tsx
+++ packages/app-game/src/pages/MergicanPage.tsx
@@ -1,8 +1,28 @@
 import { useEffect, useState } from "react";
 export function MergicanPage(props: { navigate: (path: string) => void }) {
   const [imageSrc, setImageSrc] = useState("/assets/items/item.steelbars.png");
+
+  const [rectX, setRectX] = useState(0); // Initial X position of the rectangle
+  const [rectY, setRectY] = useState(0); // Initial Y position of the rectangle
+  const [isDragging, setIsDragging] = useState(false);
+
   const handleClick = () => {
     setImageSrc("/assets/items/item.gears.png"); // Change the image source on click
+  };
+
+  const handleMouseDown = () => {
+    setIsDragging(true);
+  };
+
+  const handleMouseUp = () => {
+    setIsDragging(false);
+  };
+
+  const handleMouseMove = (e: MouseEvent) => {
+    if (isDragging) {
+      setRectX(e.clientX);
+      setRectY(e.clientY);
+    }
   };
 
   useEffect(() => {
@@ -10,18 +30,27 @@ export function MergicanPage(props: { navigate: (path: string) => void }) {
     const canvas = document.querySelector("canvas");
     const ctx = canvas.getContext("2d");
 
-    // Set canvas size
-    canvas.width = 100;
-    canvas.height = 100;
-    // Load texture image using current imageSrc state
+    // canvas.width = 400;
+    // canvas.height = 400;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
     const texture = new Image();
     texture.src = imageSrc;
 
-    // Draw texture
     texture.onload = () => {
-      ctx.drawImage(texture, 0, 0, 20, 20); // Draw texture on canvas
+      ctx.drawImage(texture, rectX, rectY, 20, 20); // Draw texture on canvas
     };
-  }, [imageSrc]);
+
+    canvas.addEventListener("mousedown", handleMouseDown);
+    canvas.addEventListener("mouseup", handleMouseUp);
+    canvas.addEventListener("mousemove", handleMouseMove);
+    return () => {
+      canvas.removeEventListener("mousedown", handleMouseDown);
+      canvas.removeEventListener("mouseup", handleMouseUp);
+      canvas.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, [imageSrc, rectX, rectY, isDragging]);
   return (
     <>
       <canvas></canvas>
