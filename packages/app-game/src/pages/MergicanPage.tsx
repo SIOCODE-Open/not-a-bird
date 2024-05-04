b086ec3b97e2235dfb4ecaa546961189efbc69a2
Frank Dierolf <frank_dierolf@web.de>
2024-05-04 16:59:29 +0200

    :zap: (app-game) add leva & increase canvas & port button to leva

--- packages/app-game/src/pages/MergicanPage.tsx
+++ packages/app-game/src/pages/MergicanPage.tsx
@@ -1,6 +1,23 @@
 import { useEffect, useState } from "react";
+import { useControls, button } from "leva";
+
 export function MergicanPage(props: { navigate: (path: string) => void }) {
   const [imageSrc, setImageSrc] = useState("/assets/items/item.steelbars.png");
+  const [, set] = useControls(
+    "a_number",
+    () => ({
+      owner: {
+        value: 4,
+        disabled: true,
+      },
+      set_number: button(() => set_number()),
+      change_picture: button(() => handleClick()),
+    }),
+    [],
+  );
+  const set_number = async () => {
+    set({ owner: 3 });
+  };
 
   const [rectX, setRectX] = useState(0); // Initial X position of the rectangle
   const [rectY, setRectY] = useState(0); // Initial Y position of the rectangle
@@ -30,8 +47,6 @@ export function MergicanPage(props: { navigate: (path: string) => void }) {
     const canvas = document.querySelector("canvas");
     const ctx = canvas.getContext("2d");
 
-    // canvas.width = 400;
-    // canvas.height = 400;
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 
@@ -39,7 +54,7 @@ export function MergicanPage(props: { navigate: (path: string) => void }) {
     texture.src = imageSrc;
 
     texture.onload = () => {
-      ctx.drawImage(texture, rectX, rectY, 20, 20); // Draw texture on canvas
+      ctx.drawImage(texture, rectX, rectY, 50, 50); // Draw texture on canvas
     };
 
     canvas.addEventListener("mousedown", handleMouseDown);
@@ -54,7 +69,6 @@ export function MergicanPage(props: { navigate: (path: string) => void }) {
   return (
     <>
       <canvas></canvas>
-      <button onClick={handleClick}>Change Image</button>
     </>
   );
 }
