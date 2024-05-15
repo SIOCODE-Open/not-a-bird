fb56a0554c2ae3b0a4591233ed5c8c168688371b
MarcelMarek <114317273+MarcelMarek@users.noreply.github.com>
2024-05-15 18:03:14 +0200

    :zap: (app-game) changed conflict with body directly used. Now div gets created with appending to body

--- packages/app-game/src/index.tsx
+++ packages/app-game/src/index.tsx
@@ -2,9 +2,8 @@ import { createRoot } from "react-dom/client";
 import { NotABirdApp } from "./NotABirdApp";
 
 async function startGame() {
-  const body = document.body;
-  const rootEl = body;
-  // body.appendChild(rootEl);
+  const rootEl = document.createElement("div");
+  document.body.appendChild(rootEl);
 
   const root = createRoot(rootEl);
   root.render(<NotABirdApp />);
