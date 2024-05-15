187f2b661e3760d397c1dfdbb3bccb332437e81b
MarcelMarek <114317273+MarcelMarek@users.noreply.github.com>
2024-05-15 18:03:45 +0200

    :zap: (app-game) changed mergican page as landing page for testing

--- packages/app-game/src/NotABirdApp.tsx
+++ packages/app-game/src/NotABirdApp.tsx
@@ -6,7 +6,6 @@ import { GameOverPage } from "./pages/GameOverPage";
 import { StarMapPage } from "./pages/StarMapPage";
 import { StarSystemPage } from "./pages/StarSystemPage";
 import { ItemsWikiPage } from "./pages/ItemsWikiPage";
-import { MarcelPage } from "./pages/MarcelPage";
 import { MergicanPage } from "./pages/MergicanPage";
 
 const pages: Array<{ path: string; component: any }> = [
@@ -16,14 +15,13 @@ const pages: Array<{ path: string; component: any }> = [
   { path: "/game", component: GamePage },
   { path: "/game-over", component: GameOverPage },
   { path: "/frank", component: FrankPage },
-  { path: "/marcel", component: MarcelPage },
   { path: "/items-wiki", component: ItemsWikiPage },
   { path: "/mergican", component: MergicanPage },
 ];
 
 export function NotABirdApp() {
-  const [currentPage, setCurrentPage] = useState("/landing-page");
-  const currentCompRef = useRef(LandingPage);
+  const [currentPage, setCurrentPage] = useState("/mergican");
+  const currentCompRef = useRef(MergicanPage);
 
   const navigate = (path: string) => {
     if (path === currentPage) {
