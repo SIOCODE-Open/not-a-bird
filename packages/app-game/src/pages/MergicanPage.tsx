c4569e83bf2628f233d65a50fb030b2d56b95cd1
Frank Dierolf <frank_dierolf@web.de>
2024-05-16 22:54:06 +0200

    :zap: (app-game) implement ContextBuilder :tada:

--- packages/app-game/src/pages/MergicanPage.tsx
+++ packages/app-game/src/pages/MergicanPage.tsx
@@ -5,9 +5,10 @@ import { useCrosshair } from "../service/CrossHairService";
 import { useResources } from "../service/ResourceService";
 import { useMouse } from "../service/MouseService";
 import { useHelpers } from "../service/HelperService";
+import { BuilderClass } from "../classes/BuilderClassExample";
 
 export function MergicanPage(props: { navigate: (path: string) => void }) {
-  const { createCrosshair } = useCrosshair();
+  const { addCrosshair } = useCrosshair();
   const { getResources, setResources } = useResources();
 
   // Resource State
@@ -32,6 +33,7 @@ export function MergicanPage(props: { navigate: (path: string) => void }) {
   useEffect(() => {
     // Get Canvas Element
     const canvas = document.querySelector("canvas");
+    const ctx = canvas.getContext("2d");
 
     // Set the Canvas Size as the whole viewport
     canvas.width = window.innerWidth;
@@ -43,6 +45,8 @@ export function MergicanPage(props: { navigate: (path: string) => void }) {
     canvas.addEventListener("mousemove", handleMouseMove);
 
     let newRessourecs: Resource[] = [];
+
+    //Loop for all ressources And draw each ressource
     resources.forEach((res_el: Resource) => {
       // If mouse in rectangle then blue
       if (getDistance(mouseX, mouseY, res_el.x, res_el.y) <= 50) {
@@ -52,22 +56,30 @@ export function MergicanPage(props: { navigate: (path: string) => void }) {
           res_el.isSelected = true;
           res_el.x = mouseX - offsetX.current;
           res_el.y = mouseY - offsetY.current;
+
           newRessourecs = resources.filter(
             (el: Resource) => el.isSelected === false,
           );
         }
       } else {
-        res_el.color = "white";
+        res_el.color = "#00000000";
         res_el.isSelected = false;
       }
 
-      // Draw Crosshair Rectangle of the Ressources on the Canvas
-      const crosshairCTX = createCrosshair(canvas, res_el, colorBorder);
+      // We use the builder Pattern and pass the the context around.
+      // f.e. Doe + Tomoato + Salami + Onions + Mushrooms + Cheese = Pizza
+      const builder = new BuilderClass(ctx, res_el.x, res_el.y, 50, 50, res_el);
+      const new_ctx = builder
+        .addSparkels("gold")
+        .addBlueCircle()
+        .addX()
+        .addCrossHair("blue")
+        .getCtx();
 
       // Add Image in rectangle
       const img = new Image();
       img.src = res_el.getCurrentImage();
-      crosshairCTX.drawImage(img, res_el.x, res_el.y, 50, 50); // Draw texture on canvas
+      new_ctx.drawImage(img, res_el.x, res_el.y, 50, 50); // Draw texture on canvas
     });
 
     // Recreate Ressources
