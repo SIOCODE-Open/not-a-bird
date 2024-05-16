2370a8badb9eae5714da147dbfeb982e59cdf49f
Frank Dierolf <frank_dierolf@web.de>
2024-05-16 21:17:25 +0200

    :zap: (app-game) cleanup MercianPage, use new Helpers

--- packages/app-game/src/pages/MergicanPage.tsx
+++ packages/app-game/src/pages/MergicanPage.tsx
@@ -1,73 +1,37 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect } from "react";
 import { Resource } from "../classes/ResourceClass";
-//!TODO BROWSER COMPATIBLITLY
-import { useBorderControls, useContractVersion1Controls, useFrontendControls } from "../components/LevaCmp";
-import { useFrank } from "../service/FrankService";
-import { BN } from "@polkadot/util";
+import { useBorderControls } from "../components/LevaCmp";
 import { useCrosshair } from "../components/CrosshairCmp";
 import { useResources } from "../service/ResourceService";
+import { useMouse } from "../service/MouseService";
+import { useHelpers } from "../service/HelperService";
 
 export function MergicanPage(props: { navigate: (path: string) => void }) {
-  // const { mint, burn, createCollection } = useUnique("");
-  const { mintMetal, mintCrystal, createMetcrys, getMetcrysCount } = useFrank();
   const { createCrosshair } = useCrosshair();
-  const { getResources, addResource, setResources } = useResources();
-
-  const [metalCount, setMetalCount] = useState(new BN(0));
-  const [crystalCount, setCrystalCount] = useState(new BN(0));
-  const [metCrysCount, setmetCrysCount] = useState("");
+  const { getResources, setResources } = useResources();
 
   // Resource State
   const resources = getResources();
 
   // Leva Controls
-  const { colorBorder, setColorBorder, borderControls } = useBorderControls();
-  const { resourcesCount, setResourcesCount, frontendControls } = useFrontendControls(resources, addResource);
-  const { contractVersion1Controls } = useContractVersion1Controls(
-    resources,
-    metalCount,
-    crystalCount,
-    metCrysCount,
-    mintMetal,
-    mintCrystal,
-    createMetcrys,
-    getMetcrysCount,
-    setMetalCount,
-    setCrystalCount,
-    setmetCrysCount
-  );
+  const { colorBorder } = useBorderControls();
 
   // Mouse
-  const [mouseX, setMouseX] = useState(0);
-  const [mouseY, setMouseY] = useState(0);
-  const offsetX = useRef(0);
-  const offsetY = useRef(0);
-
-  const [isMouseDown, setIsMouseDown] = useState(false);
-  const handleMouseDown = (e: MouseEvent) => {
-    resources.forEach((res_el: Resource) => {
-      if (getDistance(e.clientX, e.clientY, res_el.x, res_el.y) <= 50) {
-        offsetX.current = e.clientX - res_el.x;
-        offsetY.current = e.clientY - res_el.y;
-      }
-    });
-    setIsMouseDown(true);
-  };
-  const handleMouseUp = (e: MouseEvent) => {
-    setIsMouseDown(false);
-  };
-  const handleMouseMove = (e: MouseEvent) => {
-    setMouseX(e.clientX);
-    setMouseY(e.clientY);
-  };
-
-  // Helpers
-  const getDistance = (x1: number, y1: number, x2: number, y2: number) => Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+  const {
+    mouseX,
+    mouseY,
+    offsetX,
+    offsetY,
+    isMouseDown,
+    handleMouseDown,
+    handleMouseUp,
+    handleMouseMove,
+  } = useMouse();
+  const { getDistance } = useHelpers();
 
   useEffect(() => {
     // Get Canvas Element
     const canvas = document.querySelector("canvas");
-    // Grab the 2D Context out of it
 
     // Set the Canvas Size as the whole viewport
     canvas.width = window.innerWidth;
@@ -88,7 +52,9 @@ export function MergicanPage(props: { navigate: (path: string) => void }) {
           res_el.isSelected = true;
           res_el.x = mouseX - offsetX.current;
           res_el.y = mouseY - offsetY.current;
-          newRessourecs = resources.filter((el: Resource) => el.isSelected === false);
+          newRessourecs = resources.filter(
+            (el: Resource) => el.isSelected === false,
+          );
         }
       } else {
         res_el.color = "white";
