a25799fa19cb7807912662c6b1a047fcac7a5bb3
Frank Dierolf <frank_dierolf@web.de>
2024-05-17 18:52:29 +0200

    :zap: (app-game) add to each cell image

--- packages/app-game/src/classes/CellClass.ts
+++ packages/app-game/src/classes/CellClass.ts
@@ -6,14 +6,18 @@ class Cell {
   y: number;
   width: number;
   height: number;
+  image: HTMLImageElement;
   constructor(effect: ImageEffect, x: number, y: number) {
     this.effect = effect;
     this.x = x;
     this.y = y;
     this.width = this.effect.cellWidth;
     this.height = this.effect.cellHeight;
+    this.image = new Image();
+    this.image.src = "/assets/items/item.corn.png";
   }
   draw(context: CanvasRenderingContext2D) {
+    context.drawImage(this.image, this.x, this.y, this.width, this.height);
     context.strokeRect(this.x, this.y, this.width, this.height);
   }
 }
