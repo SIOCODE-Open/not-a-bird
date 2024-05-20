a305487d5f633f6839c700bcbb73d43473258ac0
Frank Dierolf <frank_dierolf@web.de>
2024-05-20 23:08:41 +0200

    :zap: (app-game) add CanvasImage Compnent, adjust FranksCard.tsx

--- packages/app-game/src/pages/FranksCard.tsx
+++ packages/app-game/src/pages/FranksCard.tsx
@@ -1,20 +1,15 @@
 import { BulmaButton } from "../components/BulmaButton";
-import { BulmaCard } from "../components/BulmaCard";
+import { CanvasImage } from "../components/CanvasImage";
 
 const FranksCard = (props: { navigate: (path: string) => { void } }) => {
   return (
     <div className="card">
       <div className="card-image">
-        <figure className="image is-16by9">
-          <iframe
-            className="has-ratio"
-            width="640"
-            height="360"
-            src="https://www.youtube.com/embed/YE7VzlLtp-4"
-            frameBorder="0"
-            allowFullScreen
-          ></iframe>
-        </figure>
+        <CanvasImage
+          width={window.innerWidth}
+          height={window.innerWidth}
+          /*imagePath=""*/
+        />
       </div>
       <div className="card-content">
         <h1>Name</h1>
