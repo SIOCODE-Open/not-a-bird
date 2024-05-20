3f3aa6ddcd307639ee8b66d495700e6b30f47439
Frank Dierolf <frank_dierolf@web.de>
2024-05-20 18:41:28 +0200

    :zap: (app-game) recreate BotondsCard in FranksCard

--- packages/app-game/src/pages/FranksCard.tsx
+++ packages/app-game/src/pages/FranksCard.tsx
@@ -1,5 +1,41 @@
+import { BulmaButton } from "../components/BulmaButton";
+import { BulmaCard } from "../components/BulmaCard";
+
 const FranksCard = (props: { navigate: (path: string) => { void } }) => {
-  return <h1>Hello World</h1>;
+  return (
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-16by9">
+          <iframe
+            className="has-ratio"
+            width="640"
+            height="360"
+            src="https://www.youtube.com/embed/YE7VzlLtp-4"
+            frameBorder="0"
+            allowFullScreen
+          ></iframe>
+        </figure>
+      </div>
+      <div className="card-content">
+        <h1>Name</h1>
+        <p>
+          <i>Description</i>
+        </p>
+        <p>5</p>
+      </div>
+      <footer className="card-footer">
+        <div className="card-footer-item">
+          <BulmaButton>Buy</BulmaButton>
+        </div>
+        <div className="card-footer-item">
+          <BulmaButton>Sacrifice</BulmaButton>
+        </div>
+        <div className="card-footer-item">
+          <BulmaButton>Send</BulmaButton>
+        </div>
+      </footer>
+    </div>
+  );
 };
 
 export { FranksCard };
