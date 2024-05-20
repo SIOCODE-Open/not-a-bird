875aa9727d6b1759a9e8d139f2e576581ddbca6b
Frank Dierolf <frank_dierolf@web.de>
2024-05-20 10:00:29 +0200

    :zap: (app-game) change background of glitter monkey

--- packages/app-game/src/pages/Plaground2.tsx
+++ packages/app-game/src/pages/Plaground2.tsx
@@ -98,7 +98,7 @@ const ParticleImage = () => {
 
       function animate() {
         ctx.globalAlpha = 0.01;
-        ctx.fillStyle = "rgb(0, 0, 255)";
+        ctx.fillStyle = "rgb(0, 0, 0)";
         ctx.fillRect(0, 0, canvas.width, canvas.height);
         ctx.globalAlpha = 0.2;
         for (let i = 0; i < particlesArray.length; i++) {
