d2ec388a3e6f202ebb96200b72c155a68720ca96
Botond János Kovács <botondjanoskovacs@gmail.com>
2024-05-22 12:49:39 +0200

    Working on the deployer

--- packages/deployer/src/index.ts
+++ packages/deployer/src/index.ts
@@ -27,6 +27,15 @@ const disconnectFromChainTask = {
     }
 };
 
+const createKeyringTask = {
+    title: 'Create Keyring',
+    task: async (ctx: any) => {
+        const keyring = new Keyring({ type: 'sr25519' });
+        const pair = keyring.addFromUri(ctx.suri, { name: 'user' });
+        ctx.keyring = keyring;
+    }
+};
+
 const deployContractTask = (
     opts: {
         contractName: string;
@@ -35,36 +44,74 @@ const deployContractTask = (
 ) => {
     return {
         title: `Deploy ${opts.contractName}`,
-        task: async (ctx: any) => {
-
+        task: async (ctx: any, task: any) => {
+            const gasLimit = ctx.api.registry.createType("WeightV2", {
+                refTime: new BN("2000000000"),
+                proofSize: new BN("200000"),
+            });
+            const storageDepositLimit = null;
+            const user = ctx.keyring.pairs.find(pair => pair.meta.name === "user");
+            const codeObject = new CodePromise(
+                ctx.api,
+                opts.contractData,
+                opts.contractData.source.wasm
+            );
+            const newCodeTx = codeObject.tx.new(
+                { gasLimit, storageDepositLimit },
+            );
+            const contractAddress = await new Promise<string>(async (resolve, reject) => {
+                const unsub = await newCodeTx.signAndSend(
+                    user as any,
+                    ({ contract, status }: any) => {
+                        if (status.isInBlock) {
+                            unsub();
+                            resolve(contract?.address!.toString());
+                        } else if (status.isFinalized) {
+                        } else if (status.isDropped || status.isInvalid || status.isUsurped) {
+                            unsub();
+                            reject();
+                        }
+                    }
+                );
+            });
+            ctx[opts.contractName] = contractAddress;
+            task.output = contractAddress;
         }
     };
 }
 
 const createGameTasks = (game: IDeployableGame) => {
-    const deployTasks: Array<ListrTask> = [];
-    deployTasks.push(
-        deployContractTask({
-            contractName: game.name,
-            contractData: game.gameContract
-        })
-    );
-    for (let elementId of Object.keys(game.elementContracts)) {
-        const element = game.elementContracts[elementId];
-        deployTasks.push(
-            deployContractTask({
-                contractName: element.name,
-                contractData: element.contract
-            })
-        );
-    }
-    return deployTasks;
+    return [
+        {
+            title: `Deploy ${game.name} game`,
+            task: (ctx: any, task: any) => {
+                const deployTasks: Array<ListrTask> = [];
+                deployTasks.push(
+                    deployContractTask({
+                        contractName: game.name,
+                        contractData: game.gameContract
+                    })
+                );
+                for (let elementId of Object.keys(game.elementContracts)) {
+                    const element = game.elementContracts[elementId];
+                    deployTasks.push(
+                        deployContractTask({
+                            contractName: `element_${elementId}`,
+                            contractData: element
+                        })
+                    );
+                }
+                return task.newListr(deployTasks);
+            }
+        }
+    ]
 };
 
 async function main() {
     const tasks: Array<ListrTask> = [
     ];
     tasks.push(connectToChainTask);
+    tasks.push(createKeyringTask);
     tasks.push(...createGameTasks(DEPLOYABLE_UNIFIERS));
     tasks.push(disconnectFromChainTask);
     const options = {
